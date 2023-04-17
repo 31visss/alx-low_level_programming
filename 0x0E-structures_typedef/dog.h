b252d78ffba17f0887917ef9f3944dd739e6be72
31visss <kimelvis13@gmail.com>
2023-04-17 07:40:08 -0700

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,17 @@
-struct dog{
+#ifndef POPPY
+#define POPPY
+/**
+ * struct dog - dog structure
+ * @name: dog name
+ * @age: age of dog
+ * @owner: dog owner
+ */
+
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif
